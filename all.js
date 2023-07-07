0f5c34ae288df6d8d2cc386ac005f2f0f5dc2999
morningsidecafe <138934177+morningsidecafe@users.noreply.github.com>
2023-07-07 15:37:35 -0600

    Add files via upload

---
+++ all.js
@@ -0,0 +1,32 @@
+ else {
+            entry.target.classList.remove('show');
+        }
+
+// to put if you need the text to disappear when not visible // 
+
+let name = 'Ola';
+
+let selectedColor = null;
+
+// we use null to empty the variable , so then we can reassign it to some other value
+//it's different from undefined value
+
+let name = 'Ola';
+let age = '23';
+
+let person = {
+    name: 'Ola',
+    age: 23
+};
+
+console.log(person);
+
+
+#myVideo {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+    z-index: -1;
+  }
